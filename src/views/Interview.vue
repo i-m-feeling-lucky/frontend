697b913731eea8c700654eded8e32f4c36598c32
yusanshi <yulei2018@gmail.com>
2020-05-01 17:59:26 +0800

    Test rtcmulticonnection

--- src/views/Interview.vue
+++ src/views/Interview.vue
@@ -6,3 +6,29 @@
     <p>Current token: {{ $route.query.token }}</p>
   </div>
 </template>
+
+
+<script lang="ts">
+import Vue from 'vue';
+import RTCMultiConnection from 'rtcmulticonnection';
+import io from 'socket.io-client';
+
+window.io = io;
+
+export default Vue.extend({
+  name: 'Interview',
+
+  mounted() {
+    const connection = new RTCMultiConnection();
+    connection.socketURL = process.env.VUE_APP_RTC_SOCKET_URL;
+
+    connection.session = {
+      audio: true,
+      video: true,
+    };
+
+    connection.openOrJoin('63');
+    console.log(connection);
+  },
+});
+</script>
