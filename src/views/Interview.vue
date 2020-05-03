8df28002221d51cdf5be5353e3a211f0972083be
yusanshi <yulei2018@gmail.com>
2020-05-04 00:42:28 +0800

    Finish screen split

--- src/views/Interview.vue
+++ src/views/Interview.vue
@@ -1,45 +1,56 @@
 <template>
   <v-app>
     <v-content>
-      <v-container fluid>
-        <v-alert icon="mdi-duck" class="caption">
-          测试房间<br />
-          [ { id: 0, HRId: 1, interviewerId: 12, intervieweeId: 18, HRToken: '<a
-            href="0?token=1f251805-b4ea-43a3-90a8-299abeb751e8"
-            >1f251805-b4ea-43a3-90a8-299abeb751e8</a
-          >', interviewerToken: '<a
-            href="0?token=60cd9fda-c9b6-4670-b5a5-f6fdb7f9ef6a"
-            >60cd9fda-c9b6-4670-b5a5-f6fdb7f9ef6a</a
-          >', intervieweeToken: '<a
-            href="0?token=563455cd-3111-42e6-9a2a-8f344dd887de"
-            >563455cd-3111-42e6-9a2a-8f344dd887de</a
-          >', startTime: Date.now() + 1000 * 60 * 24, length: 40, }, { id: 1,
-          HRId: 2, interviewerId: 8, intervieweeId: 4, HRToken: '<a
-            href="1?token=7e4e8cdf-8c4a-4d60-9305-6ce031e43fbd"
-            >7e4e8cdf-8c4a-4d60-9305-6ce031e43fbd</a
-          >', interviewerToken: '<a
-            href="1?token=bb1105e9-4da7-4097-929f-1ea0f3b1a8df"
-            >bb1105e9-4da7-4097-929f-1ea0f3b1a8df</a
-          >', intervieweeToken: '<a
-            href="1?token=bfe61c90-66b4-4f3f-a3ee-cf6504ce82ed"
-            >bfe61c90-66b4-4f3f-a3ee-cf6504ce82ed</a
-          >', startTime: Date.now() - 1000 * 60 * 24, length: 35, }, { id: 2,
-          HRId: 5, interviewerId: 22, intervieweeId: 11, HRToken: '<a
-            href="2?token=141c83b0-294e-4214-89db-09edfc3b2704"
-            >141c83b0-294e-4214-89db-09edfc3b2704</a
-          >', interviewerToken: '<a
-            href="2?token=47fc8855-62fb-45ec-8588-4563a1edd837"
-            >47fc8855-62fb-45ec-8588-4563a1edd837</a
-          >', intervieweeToken: '<a
-            href="2?token=9664eb59-5fff-40a9-ad66-1936d9d06ec6"
-            >9664eb59-5fff-40a9-ad66-1936d9d06ec6</a
-          >', startTime: Date.now() + 1000 * 60 * 2, length: 40, }, ]
-        </v-alert>
-      </v-container>
+      <golden-layout :hasHeaders="false" class="hscreen">
+        <gl-row>
+          <gl-col>
+            <gl-component
+              >代码<br />测试房间<br />
+              [ { id: 0, HRId: 1, interviewerId: 12, intervieweeId: 18, HRToken:
+              '<a href="0?token=1f251805-b4ea-43a3-90a8-299abeb751e8"
+                >1f251805-b4ea-43a3-90a8-299abeb751e8</a
+              >', interviewerToken: '<a
+                href="0?token=60cd9fda-c9b6-4670-b5a5-f6fdb7f9ef6a"
+                >60cd9fda-c9b6-4670-b5a5-f6fdb7f9ef6a</a
+              >', intervieweeToken: '<a
+                href="0?token=563455cd-3111-42e6-9a2a-8f344dd887de"
+                >563455cd-3111-42e6-9a2a-8f344dd887de</a
+              >', startTime: Date.now() + 1000 * 60 * 24, length: 40, }, { id:
+              1, HRId: 2, interviewerId: 8, intervieweeId: 4, HRToken: '<a
+                href="1?token=7e4e8cdf-8c4a-4d60-9305-6ce031e43fbd"
+                >7e4e8cdf-8c4a-4d60-9305-6ce031e43fbd</a
+              >', interviewerToken: '<a
+                href="1?token=bb1105e9-4da7-4097-929f-1ea0f3b1a8df"
+                >bb1105e9-4da7-4097-929f-1ea0f3b1a8df</a
+              >', intervieweeToken: '<a
+                href="1?token=bfe61c90-66b4-4f3f-a3ee-cf6504ce82ed"
+                >bfe61c90-66b4-4f3f-a3ee-cf6504ce82ed</a
+              >', startTime: Date.now() - 1000 * 60 * 24, length: 35, }, { id:
+              2, HRId: 5, interviewerId: 22, intervieweeId: 11, HRToken: '<a
+                href="2?token=141c83b0-294e-4214-89db-09edfc3b2704"
+                >141c83b0-294e-4214-89db-09edfc3b2704</a
+              >', interviewerToken: '<a
+                href="2?token=47fc8855-62fb-45ec-8588-4563a1edd837"
+                >47fc8855-62fb-45ec-8588-4563a1edd837</a
+              >', intervieweeToken: '<a
+                href="2?token=9664eb59-5fff-40a9-ad66-1936d9d06ec6"
+                >9664eb59-5fff-40a9-ad66-1936d9d06ec6</a
+              >', startTime: Date.now() + 1000 * 60 * 2, length: 40, }, ]
+            </gl-component>
+            <gl-component>白板 </gl-component>
+          </gl-col>
+          <gl-col>
+            <gl-row>
+              <gl-component>视频</gl-component>
+              <gl-component>视频</gl-component>
+            </gl-row>
+            <gl-component>聊天</gl-component>
+          </gl-col>
+        </gl-row>
+      </golden-layout>
     </v-content>
   </v-app>
 </template>
-
 
 <script lang="ts">
 import Vue from 'vue';
@@ -48,6 +59,7 @@ import RTCMultiConnection from 'rtcmulticonnection';
 import io from 'socket.io-client';
 import { dummyVerify } from '@/utils/dummyInterview';
 import roleMap from '@/utils/roleMap';
+import 'golden-layout/src/css/goldenlayout-translucent-theme.css';
 
 window.io = io;
 
@@ -105,3 +117,10 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style>
+.hscreen {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
