a455d2bd0d71182d110a53c67c6ab471bd5580b6
yusanshi <yulei2018@gmail.com>
2020-05-05 18:51:19 +0800

    Finish chatting

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import Vue from 'vue';
-import vgl from 'vue-golden-layout';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-import 'jquery';
 
 Vue.config.productionTip = false;
-
-Vue.use(vgl);
 
 new Vue({
   router,
