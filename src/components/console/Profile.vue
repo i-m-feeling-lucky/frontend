564a848406dc34d95afebc1405f18bedca34c19b
ipython3 <jackweller@163.com>
2020-06-16 22:18:33 +0800

    Add profile page

--- src/components/console/Profile.vue
+++ src/components/console/Profile.vue
@@ -1,11 +1,46 @@
 <template>
   <div>
-    <v-card>
-      <v-card-text class="text--primary">
-        个人信息（高优先级）
-        <br />
-        {{ getUser }}
-      </v-card-text>
+    <v-card
+      max-width="500"
+      class="mx-auto"
+    >
+      <v-img
+        src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+        height="200px"
+        dark
+      >
+        <v-row align="end" class="fill-height">
+          <v-col class="py-8">
+            <div class="text-h5 px-5">个人信息</div>
+          </v-col>
+        </v-row>
+      </v-img>
+
+      <v-list two-line>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon color="indigo">mdi-account-circle-outline</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ chineseRole }}</v-list-item-title>
+            <v-list-item-subtitle>身份</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider inset></v-divider>
+
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon color="indigo">mdi-email-outline</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ getUser.email }}</v-list-item-title>
+            <v-list-item-subtitle>注册邮箱</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-card>
   </div>
 </template>
@@ -21,6 +56,10 @@ export default Vue.extend({
   },
   computed: {
     ...mapGetters(['getUser']),
+    chineseRole() {
+      const translation = ['管理员', 'HR', '面试官'];
+      return translation[this.getUser.role];
+    },
   },
 });
 </script>
